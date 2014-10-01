cf70ab60d5ec2dd08d907cb8203521a762973565
benjamin <benjamin@boerngen-schmidt.de>
2014-10-01 17:34:43 +0200

    added logging refs #2 added database module

---
+++ code/builder/simulation_environment.py
@@ -0,0 +1,17 @@
+'''
+Created on 28.09.2014
+
+@author: Benjamin
+'''
+
+class SimulationEnvironmentBuilder(object):
+    '''
+    Builds and initializes the simulation environment
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
