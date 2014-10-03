48ee591647f24735a530aae6a688ca58ab3b86f9
Benjamin Börngen-Schmidt <benjamin@boerngen-schmidt.de>
2014-10-03 20:33:04 +0200

    Added config for osm2po Ignore py-mysql2pgsql.yml config file & python2 virtual env Fix python3 setup

--- setup/02_setup python3.sh
+++ setup/02_setup python3.sh
@@ -9,7 +9,7 @@ cd $BASE
 infoMsg "Creating Python 3 Virtual Environment"
 python3 -m venv --without-pip --copies python3-venv
 wget -P $TMPDIR https://pypi.python.org/packages/source/s/setuptools/setuptools-5.7.tar.gz#md5=81f980854a239d60d074d6ba052e21ed > /dev/null
-tar xfz $TMPDIR/setuptools-5.7.tar.gz > /dev/null
+tar xfz $TMPDIR/setuptools-5.7.tar.gz -C $TMPDIR > /dev/null
 
 infoMsg "Activating Python 3 Virtual Environment"
 source $BASE/python3-venv/bin/activate
