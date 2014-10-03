13e4687c6a87fb78ada3349eec2986f22f78c2ef
Benjamin Börngen-Schmidt <benjamin@boerngen-schmidt.de>
2014-10-03 20:25:57 +0200

    fix on de_osm_roads post-import sql

--- config/postgresql/post-import_de_osm_roads.sql
+++ config/postgresql/post-import_de_osm_roads.sql
@@ -1,4 +1,6 @@
 DROP INDEX IF EXISTS de_osm_roads_highway_index;
 
-﻿CREATE INDEX de_osm_roads_highway_index
-   ON de_osm_roads (highway ASC NULLS LAST);
+CREATE INDEX de_osm_roads_highway_index
+   ON de_osm_roads 
+   USING btree
+   (highway ASC NULLS LAST);
