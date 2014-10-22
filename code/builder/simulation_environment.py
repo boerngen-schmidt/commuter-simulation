4b40d39d9f80213a8f70022619366b915c767561
Benjamin Börngen-Schmidt <benjamin@boerngen-schmidt.de>
2014-10-22 20:46:36 +0200

    SQL Statement

--- code/builder/simulation_environment.py
+++ code/builder/simulation_environment.py
@@ -3,6 +3,7 @@ Created on 28.09.2014
 
 @author: Benjamin
 '''
+import logging
 
 class SimulationEnvironmentBuilder(object):
     '''
@@ -14,9 +15,20 @@ class SimulationEnvironmentBuilder(object):
         '''
         Constructor
         '''
-        self.logger = 
-        SELECT * FROM de_commuter_gemeinden WHERE ST_WithIn(geom, (SELECT geom FROM de_commuter_kreise WHERE name LIKE 'Vogelsberg%'))
+        self.logger = logging.getLogger('spritsim.EnvironmentBuilder')
         
     def buildEvnvironment(self):
+        '''
+        Build an environment for the simulation
+        '''
+        
+        pass
+    
+    def __getGemeindenInKreis(self):
+        '''
+        Get all the Gemeinden within a certain Kreis
+        '''
+        sql = 'SELECT * FROM de_commuter_gemeinden WHERE ST_WithIn(geom, (SELECT geom FROM de_commuter_kreise WHERE name LIKE \'Vogelsberg%\'))'
+        pass
         
         
