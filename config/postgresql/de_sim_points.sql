4919d0bae2fcb382872c53bc1eb64c2a44c91cb7
Benjamin Börngen-Schmidt <benjamin@boerngen-schmidt.de>
2014-11-09 01:40:03 +0100

    Removed foreign key from de_sim_points. fixes #13

--- config/postgresql/de_sim_points.sql
+++ config/postgresql/de_sim_points.sql
@@ -1,10 +1,12 @@
+BEGIN;
 CREATE TYPE e_sim_point AS ENUM ('start', 'end');
+END;
 
 DROP TABLE IF EXISTS de_sim_points;
 CREATE TABLE de_sim_points
 (
   id serial PRIMARY KEY,
-  parent_geometry varchar(12) REFERENCES de_shp (rs),
+  parent_geometry varchar(12),
   point_type e_sim_point
 );
 
