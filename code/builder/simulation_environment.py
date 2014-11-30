4e2d8af5bdbb9cc559d04b61056ba1f2850d9231
Benjamin Börngen-Schmidt <benjamin@boerngen-schmidt.de>
2014-11-30 21:59:46 +0100

    Finally working SQL for Kreise fixes #20

--- code/builder/simulation_environment.py
+++ code/builder/simulation_environment.py
@@ -31,11 +31,16 @@ def main():
         """
         n = [rec.outgoing, rec.incoming, rec.within, rec.within]
         t = ['start', 'end', 'within_start', 'within_end']
-        polygon = loads(bytes(rec.geom_b))
-        [work_queue.put(Command(rec.rs, rec.name, polygon, amount, p_type)) for amount, p_type in zip(n, t) ]
+        try:
+            polygon = loads(bytes(rec.geom_b))
+        except TypeError:
+            logging.error('Bad record: rs: %s, name: %s values: %s', rec.rs, rec.name, n)
+            return
+        [work_queue.put(Command(rec.rs, rec.name, polygon, amount, p_type)) for amount, p_type in zip(n, t)]
 
     start = time.time()
     with database.get_connection() as con:
+        logging.info('Executing query for Gemeinden ...')
         cur = con.cursor(cursor_factory=NamedTupleCursor)
         sql = 'SELECT ' \
               's.rs              AS rs, ' \
@@ -49,47 +54,52 @@ def main():
               '  SELECT rs, gen, ST_Union(geom) AS geom FROM de_shp_gemeinden ' \
               '  WHERE rs IN (SELECT rs FROM de_commuter_gemeinden) GROUP BY rs, gen) s USING (rs) '
         cur.execute(sql)
+        con.commit()
         [add_command(rec) for rec in cur.fetchall()]
 
-        sql = 'SELECT ' \
-              'k.rs                                       AS rs,' \
-              'k.gen                                      AS name,' \
-              '(ck.incoming-sums.incoming)                AS incoming, ' \
-              '(ck.outgoing-sums.outgoing)                AS outgoing, ' \
-              '(ck.within-sums.within)                    AS within, ' \
-              'ST_AsEWKB(ST_Difference(k.geom, geo.geom)) AS geom_b ' \
-              'FROM de_commuter_kreise ck  ' \
-              'INNER JOIN ( ' \
-              '	SELECT rs,gen,ST_Union(geom) AS geom FROM de_shp_kreise WHERE rs IN (SELECT rs FROM de_shp_kreise GROUP BY rs HAVING COUNT(rs) > 1) GROUP BY rs,gen ' \
-              '	UNION ' \
-              '	SELECT rs,gen,geom FROM de_shp_kreise WHERE rs NOT IN (SELECT rs FROM de_shp_kreise GROUP BY rs HAVING COUNT(rs) > 1) ' \
-              ') k ON (ck.rs=k.rs) ' \
-              'LEFT OUTER JOIN ( ' \
-              '  SELECT DISTINCT ' \
-              '	 k.rs     AS rs, ' \
-              '  geo.geom AS geom ' \
-              '  FROM de_shp_kreise AS k ' \
-              '  LEFT JOIN ( ' \
-              '	   SELECT ' \
-              '    ST_Union(g.geom) AS geom, SUBSTRING(g.rs FOR 5) AS rs ' \
-              '    FROM de_shp_gemeinden AS g ' \
-              '    INNER JOIN de_commuter_gemeinden c ON c.rs = g.rs ' \
-              '    GROUP BY SUBSTRING(g.rs FOR 5) ' \
-              '	 ) geo ON (k.rs=geo.rs) ' \
-              ') geo ON (geo.rs=ck.rs) ' \
-              'LEFT OUTER JOIN ( ' \
-              '	SELECT ' \
-              '	SUBSTRING(rs FOR 5) AS rs, ' \
-              '	SUM(incoming)       AS incoming, ' \
-              '	SUM(within)         AS within, ' \
-              '	SUM(outgoing)       AS outgoing ' \
-              '	FROM de_commuter_gemeinden ' \
-              '	GROUP BY SUBSTRING(rs FOR 5) ' \
+        logging.info('Executing query for Kreise ...')
+        sql = 'SELECT  ' \
+              'k.rs                                       AS rs, ' \
+              'k.gen                                      AS name, ' \
+              '(ck.incoming-COALESCE(sums.incoming, 0))   AS incoming,  ' \
+              '(ck.outgoing-COALESCE(sums.outgoing, 0))   AS outgoing,  ' \
+              '(ck.within-COALESCE(sums.within, 0))       AS within, ' \
+              'ST_AsEWKB(ST_Difference(k.geom, geo.geom)) AS geom_b  ' \
+              'FROM de_commuter_kreise ck   ' \
+              'INNER JOIN (  ' \
+              '  SELECT rs,gen,ST_Union(geom) AS geom FROM de_shp_kreise WHERE rs IN (SELECT rs FROM de_shp_kreise GROUP BY rs HAVING COUNT(rs) > 1) GROUP BY rs,gen  ' \
+              '  UNION  ' \
+              '  SELECT rs,gen,geom FROM de_shp_kreise WHERE rs NOT IN (SELECT rs FROM de_shp_kreise GROUP BY rs HAVING COUNT(rs) > 1)  ' \
+              ') k ON (ck.rs=k.rs)  ' \
+              'LEFT JOIN (  ' \
+              '  SELECT DISTINCT  ' \
+              '  k.rs     AS rs,  ' \
+              '  CASE WHEN geo.geom IS NULL  ' \
+              '    THEN ST_GeomFromText(\'POLYGON EMPTY\',4326) ' \
+              '    ELSE geo.geom ' \
+              '  END AS geom ' \
+              '  FROM de_shp_kreise AS k  ' \
+              '  LEFT JOIN (  ' \
+              '    SELECT  ' \
+              '    ST_Union(g.geom) AS geom, SUBSTRING(g.rs FOR 5) AS rs  ' \
+              '    FROM de_shp_gemeinden AS g  ' \
+              '    INNER JOIN de_commuter_gemeinden c ON c.rs = g.rs  ' \
+              '    GROUP BY SUBSTRING(g.rs FOR 5)  ' \
+              '  ) geo ON (k.rs=geo.rs)  ' \
+              ') geo ON (geo.rs=ck.rs)  ' \
+              'LEFT OUTER JOIN (  ' \
+              '  SELECT  ' \
+              '  SUBSTRING(rs FOR 5) AS rs,  ' \
+              '  SUM(incoming)       AS incoming,  ' \
+              '  SUM(within)         AS within,  ' \
+              '  SUM(outgoing)       AS outgoing  ' \
+              '  FROM de_commuter_gemeinden  ' \
+              '  GROUP BY SUBSTRING(rs FOR 5)  ' \
               ') sums ON (sums.rs = ck.rs) '
 
         cur.execute(sql)
+        con.commit()
         [add_command(rec) for rec in cur.fetchall()]
-
     logging.info('Finished filling work queue. Time: %s', time.time() - start)
 
     processes = []
