cf13d051243857e589d341566cdf8215c93047aa
Benjamin Börngen-Schmidt <benjamin@boerngen-schmidt.de>
2014-12-01 23:23:06 +0100

    Added boolean value to de_sim_points table to indicate wether the point is used in a route or not refs #18 Code for matching points refs #17

--- code/builder/process_point_matcher.py
+++ code/builder/process_point_matcher.py
@@ -14,79 +14,117 @@ If does route match the wanted conditions?
 Else choose another point
 """
 import logging
-from multiprocessing import Process
+from multiprocessing import Process, Queue
+from threading import Thread
 
 from helper import database
 
 
-class CommuterDistribution(object):
-    def __init__(self):
-
-
+commuter_distribution = {'01': [],
+                         '02': [],
+                         '03': [],
+                         '04': [],
+                         '05': [],
+                         '06': [],
+                         '07': [],
+                         '08': [],
+                         '09': [],
+                         '10': [],
+                         '11': [],
+                         '12': [],
+                         '13': [],
+                         '14': [],
+                         '15': [],
+                         '16': []}
 
 
 class PointMatcherProcess(Process):
     """
     Point Matcher class
     """
+
     def __init__(self):
         self.logging = logging.getLogger(self.__name__)
+        self.point_q = Queue()
+        self.matcher_threads = 3
 
     def run(self):
         with database.get_connection() as conn:
             with conn.cursor() as cur:
-                cur.execute('select * from de_sim_points WHERE point_type = %s offset random() * (select count(*) from de_sim_points WHERE point_type=%s) limit 1 ;', ('start',))
-                start = cur.fetchone()
+                cur.execute('SELECT rs FROM de_commuter')
+                rs = [rec[0] for rec in cur.fetchall()]
 
-                cur.execute('select * from de_sim_points WHERE point_type = %s offset random() * (select count(*) from de_sim_points WHERE point_type=%s) limit 1 ;', ('end',))
-                end = cur.fetchone()
+        while len(rs) > 0:
+            current_rs = rs.pop()
+            self.logging.info('Start matching for %s', current_rs)
+            self.fill_queue(current_rs)
+            threads = []
+            for i in range(self.matcher_threads):
+                name = 'Matcher Thread %s' % i
+                t = PointMatcherThread(self.point_q)
+                t.setName(name)
+                threads.append(t)
+                t.start()
 
-    def fetch_start_point(self):
-        with database.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute('SELECT * FROM de_sim_points_start offset random() * (select count(*) from de_sim_points) limit 1 ;', ('start',))
-                start = cur.fetchone()
-                return start
+            for t in threads:
+                t.join()
 
-    def fetch_end_point(self):
-        with database.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute('SELECT * FROm de_sim_points_end offset random() * (select count(*) from de_sim_points WHERE point_type=%s) limit 1 ;', ('end',))
-                end = cur.fetchone()
-                return end
-
-        # with database.get_connection() as conn:
-        #     cur = conn.cursor()
-        #     cur.execute('SELECT rs FROM de_commuter_gemeinden')
-        #     gemeinden = cur.fetchall()
-        #
-        # while gemeinde in gemeinden:
-        #     with database.get_connection() as conn:
-        #
+            self.logging.info('Finished matching for %s', current_rs)
 
     def match_within(self):
         with database.get_connection() as conn:
             cur = conn.cursor()
-            cur.execute('SELECT p.* FROM de_sim_points p WHERE point_type=%s AND NOT EXISTS (SELECT 1 FROM de_sim_routes r WHERE p.id == r.start_point)', ('within_start', ))
+            cur.execute(
+                'SELECT p.* FROM de_sim_points p WHERE point_type=%s AND NOT EXISTS (SELECT 1 FROM de_sim_routes r WHERE p.id == r.start_point)',
+                ('within_start', ))
 
-    def match(self):
 
-WITH x AS (
-    SELECT geom, parent_geometry
-    FROM de_sim_points
-    WHERE point_type = 'start' AND id = %s
-)
-SELECT p.*, St_Distance(p.geom, x.geom) as distance
-FROM de_sim_points p, x
-WHERE
-  ST_DWithin(p.geom, x.geom, %s)
-  AND p.parent_geometry != x.parent_geometry
-  AND p.point_type = 'end'
-  AND St_Distance(p.geom, x.geom) > %s
-ORDER BY distance
+    def fill_queue(self, rs):
+        with database.get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT id FROM de_sim_points_within_start WHERE parent_geometry = %s', (rs, ))
+                conn.commit()
+                [self.point_q.put(p[0]) for p in cur.fetchall()]
 
-Find nearest point
-            SELECT * FROM de_2po_4pgr_vertices_pgr,
-(SELECT ST_Transform(geom, 4326) as geom from de_sim_points WHERE point_type = 'start' offset random() * (select count(*) from de_sim_points WHERE point_type='start') limit 1) as start
-ORDER BY the_geom <-> start.geom
-LIMIT 1;
+
+class PointMatcherThread(Thread):
+    def __init__(self, q: Queue):
+        """
+
+        :param q: Queue with ID of points
+        :return:
+        """
+        Thread.__init__(self)
+        self.q = q
+        self.logging = logging.getLogger(self.__name__)
+
+    def run(self):
+        while not self.q.emtpy():
+            p_id = self.q.get()
+            while True:
+                with database.get_connection() as conn:
+                    cur = conn.cursor()
+                    sql = 'WITH s AS (SELECT * FROM de_sim_points_start WHERE id = %(start)s) ' \
+                          'SELECT s.id AS start, e.id AS destination ' \
+                          'FROM de_sim_points_end AS e, s ' \
+                          'WHERE ST_DWithin(e.geom::geography, s.geom::geography, %(max_d)s) ' \
+                          '  AND e.parent_geometry = s.parent_geometry ' \
+                          '  AND ST_Distance(e.geom::geography, s.geom::geography) > %(min_d)s ' \
+                          '  AND e.used = false ' \
+                          'ORDER BY RANDOM() ' \
+                          'LIMIT 1 ' \
+                          'FOR UPDATE'
+                    cur.execute(sql, {'start': p_id, 'max_d': 10000, 'min_d': 2000})
+                    if cur.rowcount == 0:
+                        self.logging.warning('No possible end point found for %s', p_id)
+                    (destination, ) = cur.fetchone()
+                    cur.execute('UPDATE de_sim_points_end SET used = true WHERE used = false AND id = %s',
+                                (destination, ))
+                    if cur.rowcount is 0:
+                        # Update did not work, rollback, find new point
+                        conn.rollback()
+                        continue
+                    cur.execute('INSERT INTO de_sim_routes (start_point, end_point) VALUES (%s, %s)',
+                                (p_id, destination,))
+                    conn.commit()
+
--- config/postgresql/de_sim_points.sql
+++ config/postgresql/de_sim_points.sql
@@ -4,6 +4,7 @@ CREATE TABLE de_sim_points
 (
   id serial NOT NULL,
   parent_geometry character varying(12),
+  used boolean DEFAULT false,
   CONSTRAINT de_sim_points_pkey PRIMARY KEY (id)
 );
 
