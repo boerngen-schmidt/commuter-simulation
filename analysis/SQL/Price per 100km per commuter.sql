3553e4979d61a3953cd2b68a72d628c4d2afae5d
benjamin <benjamin@boerngen-schmidt.de>
2015-08-31 20:20:50 +0200

    Updated the filtering of autobahn filing stations

---
+++ analysis/SQL/Price per 100km per commuter.sql
@@ -0,0 +1,17 @@
+﻿SELECT c_id, noapp::float/k.driven_distance*100 AS ppkm_noapp, app::float/n.driven_distance*100 AS ppkm_app FROM (
+	(
+	  SELECT c_id, driven_distance FROM de_sim_data_commuter WHERE NOT rerun ORDER BY c_id
+	) c1
+	LEFT JOIN LATERAL (
+	  SELECT SUM(amount*price) as noapp FROM de_sim_data_refill WHERE c_id = c1.c_id AND NOT rerun
+	) p1 ON TRUE
+) k
+LEFT JOIN (
+	(
+	  SELECT c_id, driven_distance FROM de_sim_data_commuter WHERE rerun ORDER BY c_id
+	) c2
+	LEFT JOIN LATERAL (
+	  SELECT SUM(amount*price) as app FROM de_sim_data_refill WHERE c_id = c2.c_id AND rerun
+	) p2 ON TRUE
+) n USING (c_id)
+WHERE app IS NOT NULL AND noapp IS NOT NULL
--- analysis/regression_PricePerLiter_fixedEffects.R
+++ analysis/regression_PricePerLiter_fixedEffects.R
@@ -15,7 +15,7 @@ dbSendQuery(con, "BEGIN")
 dbSendQuery(con, "CREATE TEMP TABLE bab_stations AS 
             SELECT id 
             FROM de_tt_stations_modified 
-            WHERE LOWER(street) ~ '(a\\d+|a \\d+|bab|autohof|rasthof)'")
+            WHERE LOWER(street) ~  '(a\\d+|a\\s+\\d+|bab|autohof|rasthof|autobahn|^a$)' OR LOWER(name) ~ 'bat'")
 dbSendQuery(con, "CREATE TEMP TABLE brands AS 
             SELECT brand, count(brand) 
             FROM de_tt_stations_modified 
