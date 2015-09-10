b183ca0640dc08ef5e6cedd5d181eda75e8aec2f
benjamin <benjamin@boerngen-schmidt.de>
2015-09-10 16:37:28 +0200

    removed old regressions

--- analysis/Fuelstation Brands.sql
+++
@@ -1,6 +0,0 @@
-SELECT brand, count(brand)
-FROM de_tt_stations_modified
-WHERE LOWER(brand) NOT IN ('bft', 'freie tankstelle')
-GROUP BY brand 
---HAVING COUNT(brand) > 200;
-ORDER BY COUNT(brand) desc;
